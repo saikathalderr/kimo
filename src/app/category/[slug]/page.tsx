7e777960b04b5a182c8e70207db175315102df32
saikathalderr <saikathalderr@gmail.com>
2023-06-01 10:58:09 +0530

    refactor: remove Categories page

--- src/app/category/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPageProps } from '@/app/types';
-
-function CategoryPage({ params }: NextPageProps) {
-  const { slug } = params;
-
-  return <p>CategoryPage: {slug}</p>;
-}
-
-export default CategoryPage;
